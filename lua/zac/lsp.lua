cdccf2b4495f795b7222da7ab1b22e9f81e1f707
zhi zeng <cursorzz@163.com>
2021-07-08 10:24:00 +0800

    add lua lps support, currently just for macOS

--- lua/zac/lsp.lua
+++ lua/zac/lsp.lua
@@ -33,10 +33,41 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] =
 --     root_dir = require'lspconfig'.util.root_pattern(".git", vim.fn.getcwd()),
 --   }
 -- }
-
 -- require'lspconfig'.emmet_ls.setup{
 --   on_attach = on_attach;
 -- }
+local sumneko_root_path = "/Users/zengzhi/Project/lua-language-server"
+local sumneko_binary = sumneko_root_path .. "/bin/macOS/lua-language-server"
+
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+
+lsp.sumneko_lua.setup {
+   cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = runtime_path,
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
 lsp.emmet_ls.setup {}
 
 lsp.vimls.setup {}
