821240bf38d8e0681449d2eaddc4e17290e2faf8
Zac zeng <zengzhilg@gmail.com>
2021-07-13 16:08:15 +0800

    add grep to qflist function

--- lua/zac/telescope.lua
+++ lua/zac/telescope.lua
@@ -1,112 +1,125 @@
-local actions = require('telescope.actions')
+local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
 local M = {}
 
-require'telescope'.setup{
+require "telescope".setup {
   defaults = {
     mappings = {
       i = {
         ["<c-j>"] = actions.move_selection_next,
         ["<c-k>"] = actions.move_selection_previous,
-        ["<esc>"] = actions.close,
+        ["<esc>"] = actions.close
       }
-    },
+    }
   },
   extensions = {
-      fzy_native = {
-          override_generic_sorter = false,
-          override_file_sorter = true,
-      }
+    fzy_native = {
+      override_generic_sorter = false,
+      override_file_sorter = true
+    }
   }
 }
-require('telescope').load_extension('fzy_native')
+require("telescope").load_extension("fzy_native")
 -- local my_mapping = function(_, map)
 --   map("i", '<c-j>', actions.move_selection_next)
 --   map("i", '<c-k>', actions.move_selection_previous)
 --   return true
 -- end
-M.search_dotfiles = function() 
+M.search_dotfiles = function()
   require("telescope.builtin").find_files(
-    require('telescope.themes').get_dropdown({
-      previewer=false,
-      prompt_title = "< VimRC >",
-      cwd = "$HOME/.config/nvim",
-    })
+    require("telescope.themes").get_dropdown(
+      {
+        previewer = false,
+        prompt_title = "< VimRC >",
+        cwd = "$HOME/.config/nvim"
+      }
+    )
   )
 end
 
 M.find_in_current_folder = function()
   local path = vim.fn.expand("%:p:h")
-  require'telescope.builtin'.find_files(
-    require('telescope.themes').get_dropdown({
-      previewer=false,
-      prompt_title = path,
-      cwd = path,
-    })
+  require "telescope.builtin".find_files(
+    require("telescope.themes").get_dropdown(
+      {
+        previewer = false,
+        prompt_title = path,
+        cwd = path
+      }
+    )
   )
 end
 
 M.find_files = function()
-  require'telescope.builtin'.find_files(
-    require('telescope.themes').get_dropdown({
-      previewer=false,
+  require "telescope.builtin".find_files(
+    require("telescope.themes").get_dropdown(
+      {
+        previewer = false
 
         -- return {
-          --   prompt = prompt
-          -- }
-          -- return {
-            --   update_finder:
-            -- }
-    })
+        --   prompt = prompt
+        -- }
+        -- return {
+        --   update_finder:
+        -- }
+      }
+    )
   )
 end
 
 M.grep_find = function()
   result = vim.fn.expand("<cword>")
 
-  if result == '' then
+  if result == "" then
     result = vim.fn.input("Grep For >")
   end
-  require('telescope.builtin').grep_string {
+  require("telescope.builtin").grep_string {
     search = result,
     -- _on_input_filter_cb = function(prompt)
     --   print(prompt)
     --   return {}
     -- end,
     attach_mappings = function(buf, map)
-      map('i', '<c-l>', function()
-        local picker = action_state.get_current_picker(buf)
-        -- print(vim.inspect(picker.manager))
-      end)
+      map(
+        "i",
+        "<c-l>",
+        function()
+          actions.send_to_qflist(buf)
+          actions.open_qflist()
+          -- print(vim.inspect(picker.manager))
+        end
+      )
       return true
     end
   }
 end
 
 M.git_files = function()
-  require'telescope.builtin'.git_files(
-    require('telescope.themes').get_dropdown({
-      previewer=false,
-      on_input_filter_cb = function(prompt)
-        local result = vim.split(prompt, " ")
-        if #result == 2 then
-          return {
-            prompt = result[2]..'.'..result[1]
-          }
-        else
-          return {
-            prompt = prompt
-          }
+  require "telescope.builtin".git_files(
+    require("telescope.themes").get_dropdown(
+      {
+        previewer = false,
+        on_input_filter_cb = function(prompt)
+          local result = vim.split(prompt, " ")
+          if #result == 2 then
+            return {
+              prompt = result[2] .. "." .. result[1]
+            }
+          else
+            return {
+              prompt = prompt
+            }
+          end
+          -- print(prompt:match("%s"))
         end
-        -- print(prompt:match("%s"))
-      end
-    })
+      }
+    )
   )
 end
 
 M.buffers = function()
-  require'telescope.builtin'.buffers(
-    require('telescope.themes').get_dropdown({previewer=false, show_all_buffers=true})
+  require "telescope.builtin".buffers(
+    require("telescope.themes").get_dropdown({previewer = false, show_all_buffers = true})
   )
 end
 
