67494cf637281e3a213229337aafff1ca1f9b0d7
Zac zeng <zengzhilg@gmail.com>
2021-07-20 17:45:05 +0800

    fix: tree.nvim caused lag

--- lua/init.lua
+++ lua/init.lua
@@ -1,83 +1,97 @@
--- require('zac.nvimux')
-require('zac.treesitter')
-require('zac.lsp')
-require('zac.compe')
-require('zac.formatter')
+-- nvim tree seems has a bug related to git
+-- which makes startup and save extremly slow, see this
+-- https://github.com/kyazdani42/nvim-tree.lua/issues/453
+vim.g.nvim_tree_git_hl = 0
+vim.g.nvim_tree_gitignore = 0
+vim.g.nvim_tree_show_icons = {
+  git = 0,
+  folders = 1,
+  files = 1
+}
 
-require('gitsigns').setup()
-require('colorizer').setup()
-require('tmux').setup({
-  copy_sync = {
-    -- enables copy sync and overwrites all register actions to
-    -- sync registers *, +, unnamed, and 0 till 9 from tmux in advance
-    enable = true,
-  },
-  navigation = {
-    -- enables default keybindings (C-hjkl) for normal mode
-    enable_default_keybindings = true,
-  },
-  resize = {
-    -- enables default keybindings (A-hjkl) for normal mode
-    enable_default_keybindings = true,
+-- require('zac.nvimux')
+--
+require("zac.treesitter")
+require("zac.lsp")
+require("zac.compe")
+require("zac.formatter")
+
+require("gitsigns").setup()
+require("colorizer").setup()
+require("tmux").setup(
+  {
+    copy_sync = {
+      -- enables copy sync and overwrites all register actions to
+      -- sync registers *, +, unnamed, and 0 till 9 from tmux in advance
+      enable = false
+    },
+    navigation = {
+      -- enables default keybindings (C-hjkl) for normal mode
+      enable_default_keybindings = true
+    },
+    resize = {
+      -- enables default keybindings (A-hjkl) for normal mode
+      enable_default_keybindings = true
+    }
   }
-})
+)
 -- require'lsp_signature'.on_attach()
 --
 -- require('neogit').setup()
-local cb = require'diffview.config'.diffview_callback
+local cb = require "diffview.config".diffview_callback
 
-require'diffview'.setup {
-  diff_binaries = false,    -- Show diffs for binaries
+require "diffview".setup {
+  diff_binaries = false, -- Show diffs for binaries
   file_panel = {
     width = 35,
-    use_icons = false        -- Requires nvim-web-devicons
+    use_icons = false -- Requires nvim-web-devicons
   },
   key_bindings = {
-    disable_defaults = false,                   -- Disable the default key bindings
+    disable_defaults = false, -- Disable the default key bindings
     -- The `view` bindings are active in the diff buffers, only when the current
     -- tabpage is a Diffview.
     view = {
-      ["<tab>"]     = cb("select_next_entry"),  -- Open the diff for the next file 
-      ["<s-tab>"]   = cb("select_prev_entry"),  -- Open the diff for the previous file
-      ["<leader>e"] = cb("focus_files"),        -- Bring focus to the files panel
-      ["<leader>b"] = cb("toggle_files"),       -- Toggle the files panel.
+      ["<tab>"] = cb("select_next_entry"), -- Open the diff for the next file
+      ["<s-tab>"] = cb("select_prev_entry"), -- Open the diff for the previous file
+      ["<leader>e"] = cb("focus_files"), -- Bring focus to the files panel
+      ["<leader>b"] = cb("toggle_files") -- Toggle the files panel.
     },
     file_panel = {
-      ["j"]             = cb("next_entry"),         -- Bring the cursor to the next file entry
-      ["k"]             = cb("prev_entry"),         -- Bring the cursor to the previous file entry.
-      ["<cr>"]          = cb("select_entry"),       -- Open the diff for the selected entry.
-      ["-"]             = cb("toggle_stage_entry"), -- Stage / unstage the selected entry.
-      ["S"]             = cb("stage_all"),          -- Stage all entries.
-      ["U"]             = cb("unstage_all"),        -- Unstage all entries.
-      ["R"]             = cb("refresh_files"),      -- Update stats and entries in the file list.
-      ["<tab>"]         = cb("select_next_entry"),
-      ["<s-tab>"]       = cb("select_prev_entry"),
-      ["<leader>e"]     = cb("focus_files"),
-      ["<leader>b"]     = cb("toggle_files"),
+      ["j"] = cb("next_entry"), -- Bring the cursor to the next file entry
+      ["k"] = cb("prev_entry"), -- Bring the cursor to the previous file entry.
+      ["<cr>"] = cb("select_entry"), -- Open the diff for the selected entry.
+      ["-"] = cb("toggle_stage_entry"), -- Stage / unstage the selected entry.
+      ["S"] = cb("stage_all"), -- Stage all entries.
+      ["U"] = cb("unstage_all"), -- Unstage all entries.
+      ["R"] = cb("refresh_files"), -- Update stats and entries in the file list.
+      ["<tab>"] = cb("select_next_entry"),
+      ["<s-tab>"] = cb("select_prev_entry"),
+      ["<leader>e"] = cb("focus_files"),
+      ["<leader>b"] = cb("toggle_files")
     }
   }
 }
 
-require('lualine').setup {
- options = {section_separators = '', component_separators = '', theme = 'wombat'};
- sections = {
-   lualine_c = {
-    {
-      'filename',
-      file_status = true, -- displays file status (readonly status, modified status)
-      path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
+require("lualine").setup {
+  options = {section_separators = "", component_separators = "", theme = "wombat"},
+  sections = {
+    lualine_c = {
+      {
+        "filename",
+        file_status = true, -- displays file status (readonly status, modified status)
+        path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
+      }
     }
   }
- }
 }
 
-require'nvim-treesitter.configs'.setup {
+require "nvim-treesitter.configs".setup {
   textsubjects = {
     enable = true,
     keymaps = {
-      ['<cr>'] = 'textsubjects-smart',
+      ["<cr>"] = "textsubjects-smart"
     }
-  },
+  }
 }
 
 -- require('base16-colorscheme').setup({
