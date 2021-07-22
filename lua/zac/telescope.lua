a4de670dbc620f2eece2482120a568768f5ddcd6
Zac zeng <zengzhilg@gmail.com>
2021-07-22 10:55:00 +0800

    global default theme

--- lua/zac/telescope.lua
+++ lua/zac/telescope.lua
@@ -1,9 +1,9 @@
 local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
 local M = {}
-
-require "telescope".setup {
-  defaults = {
+local default_theme =
+  require("telescope.themes").get_dropdown(
+  {
     mappings = {
       i = {
         ["<c-j>"] = actions.move_selection_next,
@@ -11,7 +11,11 @@ require "telescope".setup {
         ["<esc>"] = actions.close
       }
     }
-  },
+  }
+)
+require("telescope").load_extension("fzy_native")
+require "telescope".setup {
+  defaults = default_theme,
   extensions = {
     fzy_native = {
       override_generic_sorter = false,
@@ -19,34 +23,30 @@ require "telescope".setup {
     }
   }
 }
-require("telescope").load_extension("fzy_native")
--- local my_mapping = function(_, map)
---   map("i", '<c-j>', actions.move_selection_next)
---   map("i", '<c-k>', actions.move_selection_previous)
---   return true
--- end
+
 M.search_dotfiles = function()
   require("telescope.builtin").find_files(
-    require("telescope.themes").get_dropdown(
-      {
-        previewer = false,
-        prompt_title = "< VimRC >",
-        cwd = "$HOME/.config/nvim"
-      }
-    )
+    {
+      previewer = false,
+      prompt_title = "< VimRC >",
+      cwd = "$HOME/.config/nvim"
+    }
   )
 end
 
+local function to_parent_folder(buf, map)
+end
+
+-- override builtins
+
 M.find_in_current_folder = function()
   local path = vim.fn.expand("%:p:h")
-  require "telescope.builtin".find_files(
-    require("telescope.themes").get_dropdown(
-      {
-        previewer = false,
-        prompt_title = path,
-        cwd = path
-      }
-    )
+  require "telescope.builtin".file_browser(
+    {
+      previewer = false,
+      prompt_title = path,
+      cwd = path
+    }
   )
 end
 
@@ -55,13 +55,6 @@ M.find_files = function()
     require("telescope.themes").get_dropdown(
       {
         previewer = false
-
-        -- return {
-        --   prompt = prompt
-        -- }
-        -- return {
-        --   update_finder:
-        -- }
       }
     )
   )
@@ -75,10 +68,6 @@ M.grep_find = function()
   end
   require("telescope.builtin").grep_string {
     search = result,
-    -- _on_input_filter_cb = function(prompt)
-    --   print(prompt)
-    --   return {}
-    -- end,
     attach_mappings = function(buf, map)
       map(
         "i",
@@ -86,7 +75,6 @@ M.grep_find = function()
         function()
           actions.send_to_qflist(buf)
           actions.open_qflist()
-          -- print(vim.inspect(picker.manager))
         end
       )
       return true
@@ -96,31 +84,27 @@ end
 
 M.git_files = function()
   require "telescope.builtin".git_files(
-    require("telescope.themes").get_dropdown(
-      {
-        previewer = false,
-        on_input_filter_cb = function(prompt)
-          local result = vim.split(prompt, " ")
-          if #result == 2 then
-            return {
-              prompt = result[2] .. "." .. result[1]
-            }
-          else
-            return {
-              prompt = prompt
-            }
-          end
-          -- print(prompt:match("%s"))
+    {
+      previewer = false,
+      on_input_filter_cb = function(prompt)
+        local result = vim.split(prompt, " ")
+        if #result == 2 then
+          return {
+            prompt = result[2] .. "." .. result[1]
+          }
+        else
+          return {
+            prompt = prompt
+          }
         end
-      }
-    )
+        -- print(prompt:match("%s"))
+      end
+    }
   )
 end
 
 M.buffers = function()
-  require "telescope.builtin".buffers(
-    require("telescope.themes").get_dropdown({previewer = false, show_all_buffers = true})
-  )
+  require "telescope.builtin".buffers({previewer = false, show_all_buffers = true})
 end
 
 return M
